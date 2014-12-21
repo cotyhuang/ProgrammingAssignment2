19ef6e2db6f9b9c95a61ccde1051c186a4980d22
coty huang <reghuang1119@gmail.com>
2014-12-22 01:34:37 +0800

    i just imtate cachemean

--- cachematrix.R
+++ cachematrix.R
@@ -2,8 +2,19 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## cache Matrix
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInv <- function(Inv) m <<- Inv
+  getInv <- function() m
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
 
 }
 
@@ -11,5 +22,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getInv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInv(m)
+  m
 }
